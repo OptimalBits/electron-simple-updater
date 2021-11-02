0f952d8da746ee3f3900c2aab84b073120d1a259
Manuel Astudillo <manuel@optimalbits.com>
2021-11-02 18:30:38 +0800

    fix: fix download file

--- src/utils/HttpClient.js
+++ src/utils/HttpClient.js
@@ -1,7 +1,14 @@
-'use strict';
+"use strict";
 
-const request = require('httpreq');
-const fs = require('fs');
+// We need this fancy way for importing node-fetch and make it work with webpack
+const _importDynamic = new Function("modulePath", "return import(modulePath)");
+
+async function fetch(...args) {
+  const { default: fetch } = await _importDynamic("node-fetch");
+  return fetch(...args);
+}
+
+const fs = require("fs");
 
 class HttpClient {
   /**
@@ -12,45 +19,38 @@ class HttpClient {
   }
 
   async getJson(url) {
-    return new Promise((resolve, reject) => {
-      request.get(url, this.getHttpOptions(), (err, response) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+    const res = await fetch(url, this.getHttpOptions());
 
-        try {
-          resolve(JSON.parse(response.body));
-        } catch (e) {
-          reject(new Error(
-            `Error while parsing '${url}'. ${e}. Data:\\n ${response.body}`
-          ));
-        }
-      });
-    });
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(await res.text());
+    }
   }
 
+  /**
+   * Downloads the given file at url into savePath.
+   *
+   */
   async downloadFile(url, savePath) {
+    const res = await fetch(url, this.getHttpOptions());
     return new Promise((resolve, reject) => {
-      const options = {
-        ...this.getHttpOptions(),
-        url,
-        method: 'GET',
-        downloadlocation: savePath,
-        allowRedirects: true,
+      const fileStream = fs.createWriteStream(savePath);
+      res.body.pipe(fileStream);
+
+      const handleError = async (err) => {
+        const stat = await fs.promises.stat(savePath);
+        if (stat.isFile) {
+          await fs.promises.unlink(savePath);
+        }
+        reject(err);
       };
 
-      request.doRequest(options, async (err, res) => {
-        if (err) {
-          return reject(err);
-        }
+      res.body.on("error", handleError);
 
-        if (res.statusCode !== 200) {
-          await fs.promises.unlink(savePath);
-          return reject(new Error(`Wrong HTTP status: ${res.statusCode}`));
-        }
-
-        resolve(savePath);
+      fileStream.on("error", handleError);
+      fileStream.on("finish", function () {
+        resolve();
       });
     });
   }
@@ -64,9 +64,10 @@ class HttpClient {
     return {
       ...options,
       headers: {
-        'User-Agent': 'electron-simple-updater',
+        "User-Agent": "electron-simple-updater",
         ...options.headers,
       },
+      redirect: "follow",
     };
   }
 }
